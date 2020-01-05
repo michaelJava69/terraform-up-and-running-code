f8beefddf633995403c98ede00d0c6e8f34f22de
michaelJava69 <michael.professional@inbox.com>
2020-01-05 18:25:26 +0000

    indeed

--- code/terraform/09-terraform-michael/multi-repo-example/live/stage/services/webserver-cluster/main.tf
+++ code/terraform/09-terraform-michael/multi-repo-example/live/stage/services/webserver-cluster/main.tf
@@ -14,7 +14,12 @@ module "webserver_cluster" {
   # Since the terraform-up-and-running-code repo is open source, we're using an HTTPS URL here. If it was a private
   # repo, we'd instead use an SSH URL (git@github.com:brikis98/terraform-up-and-running-code.git) to leverage SSH auth
   # source = "github.com/brikis98/terraform-up-and-running-code//code/terraform/04-terraform-module/module-example/modules/services/webserver-cluster?ref=v0.1.0"
-  source = "github.com/michaelJava69/terraform-up-and-running-code//code/terraform/09-terraform-michael/module-example/modules/services/webserver-cluster?ref=v0.1.0"
+
+  # for 0.5 loops and expressions
+  source = "github.com/michaelJava69/terraform-up-and-running-code//code/terraform/09-terraform-michael/module-example/modules/services/webserver-cluster?ref=v0.4.0"
+  
+  # for 0.5 zero downtime
+  source = "github.com/michaelJava69/terraform-up-and-running-code//code/terraform/09-terraform-michael/module-example/modules/services/webserver-cluster?ref=v0.5.0"
 
   cluster_name           = var.cluster_name
   db_remote_state_bucket = var.db_remote_state_bucket
@@ -23,6 +28,11 @@ module "webserver_cluster" {
   instance_type = "t2.micro"
   min_size      = 2
   max_size      = 2
+
+  # addtions for chapter 5 loops and expressions
+  enable_autoscaling   = false
+  enable_new_user_data = true
+
 }
 
 resource "aws_security_group_rule" "allow_testing_inbound" {
